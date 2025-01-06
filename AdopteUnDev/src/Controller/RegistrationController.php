aefed69627ad27aef3c6ba39207470af1843e35a
Andy Bongo <andy-dufrenne.bongo@etudiant.univ-rennes.fr>
2025-01-06 22:52:29 +0100

    commit register

--- AdopteUnDev/src/Controller/RegistrationController.php
+++ AdopteUnDev/src/Controller/RegistrationController.php
@@ -42,7 +42,7 @@ class RegistrationController extends AbstractController
             return $this->redirectToRoute('app_login');
         }
 
-         return $this->render('registration/register.html.twig', [
+         return $this->render('sign_up/index.html.twig', [
             'form' => $form->createView(),
          ]);
     }
